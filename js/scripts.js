96e0b149e7e4e375bab1c205702a220537d23cc0
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-29 13:33:19 -0800

    write method to generate shortDescription of each pizza that can be displayed as a string

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,41 @@ function Pizza(size, crust, cheese, vegetables, meats) {
 }
 
 Pizza.prototype.calculatePrice = function() {
-  this.price = 10;
+  this.price = 10; //base price
+  //size price
   if (this.size === 'small') {
     this.price -= 3;
   } else if (this.size === 'large') {
     this.price += 3;
   }
+  //crust price
   if (this.crust === 'stuffed') {
     this.price += 3;
   } else if (this.crust === 'pan') {
     this.price += 2;
   }
+  //cheese price
   if (this.cheese === 'no') {
     this.price -= 1;
   }
+  //vegetables price
   for (var i = 0; i < this.vegetables.length; i++) {
     this.price += 1.5;
   }
+  //meat price
   for (var i = 0; i < this.meats.length; i++) {
     this.price += 2.5;
   }
 }
 
+Pizza.prototype.shortDescription = function() {
+  return "A " + this.size + " " + this.crust + " pizza: $" + this.price;
+}
+
 $(document).ready(function() {
   $('#order-form').submit(function(event) {
     event.preventDefault();
+    //store form inputs in variable to create new pizza
     var size = $('input[name=size]:checked').val();
     var crust = $('input[name=crust]:checked').val();
     var cheese = $('input[name=cheese]:checked').val();
@@ -42,12 +52,12 @@ $(document).ready(function() {
     var meats = $('input:checkbox[name=meats]:checked').map(function() {
         return this.value;
       }).get();
-
     var newPizza = new Pizza(size, crust, cheese, vegetables, meats);
+    //calculate price based on user input
     newPizza.calculatePrice();
-
-    $('#price-display').text('Total: $' + newPizza.price);
-
+    //display each pizza as a list item
+    $('#pizza-list').append('<li>' + newPizza.shortDescription() + '</li>');
+    //reset form buttons back to default
     document.getElementById("order-form").reset();
   });
 
