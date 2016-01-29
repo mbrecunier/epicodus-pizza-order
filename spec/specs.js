3fc9ef333ced996d8d65c77e7d37265e92b057cf
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-29 10:37:38 -0800

    change calculatePrice spec to test for no cheese

--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,15 @@
 describe('Pizza', function() {
   it('creates a new Pizza object with the following properties', function() {
-    var testPizza = new Pizza('large', 'pan', true, ['spinach', 'olives'], ['pepperoni', 'bacon']);
+    var testPizza = new Pizza('large', 'pan', 'yes', ['spinach', 'olives'], ['pepperoni', 'bacon']);
     expect(testPizza.size).to.equal('large');
     expect(testPizza.crust).to.equal('pan');
-    expect(testPizza.cheese).to.equal(true);
+    expect(testPizza.cheese).to.equal('yes');
     expect(testPizza.vegetables).to.eql(['spinach', 'olives']);
     expect(testPizza.meats).to.eql(['pepperoni', 'bacon']);
   });
   it('has a method that will calculate price', function() {
-    var testPizza = new Pizza('small', 'pan', true, ['bell peppers', 'artichoke hearts', 'spinach'], ['sausage']);
+    var testPizza = new Pizza('small', 'pan', 'no', ['bell peppers', 'artichoke hearts', 'spinach'], ['sausage']);
     testPizza.calculatePrice();
-    expect(testPizza.price).to.equal(16);
+    expect(testPizza.price).to.equal(15);
   });
 });
