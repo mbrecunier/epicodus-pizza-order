a6b6b02d151f71ee5d629fe07d1d55746ab36758
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-29 09:07:15 -0800

    write Pizza object constructor

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Pizza(size, crust, cheese, vegetables, meats) {
+  this.size = size;
+  this.crust = crust;
+  this.cheese = cheese;
+  this.vegetables = vegetables;
+  this.meats = meats;
+}
