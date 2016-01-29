b09dc49798cb2949b13e3821b8e42eead2599aa0
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-29 09:28:53 -0800

    write calculatePrice method for Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,32 @@ function Pizza(size, crust, cheese, vegetables, meats) {
   this.cheese = cheese;
   this.vegetables = vegetables;
   this.meats = meats;
+  this.price;
+}
+
+Pizza.prototype.calculatePrice = function() {
+  this.price = 10;
+  if (this.size === 'small') {
+    this.price -= 3;
+  } else if (this.size === 'large') {
+    this.price += 3;
+  }
+
+  if (this.crust === 'stuffed') {
+    this.price += 3;
+  } else if (this.crust === 'pan') {
+    this.price += 2;
+  }
+
+  if (this.cheese === false) {
+    this.price -= 1;
+  }
+
+  for (var i = 0; i < this.vegetables.length; i++) {
+    this.price += 1.5;
+  }
+
+  for (var i = 0; i < this.meats.length; i++) {
+    this.price += 2.5;
+  }
 }
